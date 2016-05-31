fa6f1fb6d88885175410f986d17bbdc5f819f6ca
Your Name <zberms@gmail.com>
2016-05-31 20:55:16 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vidshare::Application.config.secret_key_base = '32618bcc748ec5e5f1535f18754d24b3957fbd6e747321c154a0e6b247fef4f70c1364baffa88e92d471b37b5faf9acd8be24e0c2490cc7afbd0bfc97f4003ef'
