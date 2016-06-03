c97a6047d5b32d92e881a3adbc922a62c75134d8
Your Name <zberms@gmail.com>
2016-06-03 16:36:05 +0000

    switch to aws for video upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  storage :aws
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -26,6 +26,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   process resize_to_fill: [800, 350]
+
   #
   # def scale(width, height)
   #   # do something
@@ -33,7 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process :resize_to_fit => [50, 50]
+  # process :resize_to_fit => [75, 75]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
